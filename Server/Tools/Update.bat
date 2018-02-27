db3f1c633864ac047b9414b05046689b3858a36e
Conan <conan_513@hotmail.com>
2018-02-27 11:40:57 +0100

    Pull from AshamaneCore
    - Spells/Warlock Fix agony soul shard proc rate
    - DB/Suramar All lot of suramar sniff data (Thanks @KittensRJerks)
    - Spells/Druid Fix Scyther of Elune (Thanks @Alysrazor)
    - DB/Suramar Fix wrong quest_poi
    - Core/SmartAI Make creature fall if SetFly false
    - Core/SmartAI Better moveFall fix
    - Scripts/Deadmines minor cleanup
    - Spells/Warlock Remove deleted spells + cleanup
    - DB/AlliedRaces Add mount at allied races creation (TempFix)

--- Server/Tools/Update.bat
+++ Server/Tools/Update.bat
@@ -9,6 +9,7 @@ cd spp-legion
 ..\git\cmd\git.exe commit -m "Custom Changes"
 ..\git\cmd\git.exe add sql
 ..\git\cmd\git.exe commit -m "Fix git sql warning"
+cls
 ..\git\cmd\git.exe pull https://github.com/conan513/spp-legion-patch.git 735
 echo.
 pause
