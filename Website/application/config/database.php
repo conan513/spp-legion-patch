b80a8ed6b7d0d8e40f71aa3bee0b323c79d0617d
Conan <conan_513@hotmail.com>
2018-03-31 19:29:56 +0200

    Missing configs because gitignore

---
+++ Website/application/config/database.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '127.0.0.1:3310',
+	'username' => 'root',
+	'password' => '123456',
+	'database' => 'blizzcms',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => 'fx_',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['auth'] = array(
+	'dsn'	=> '',
+	'hostname' => '127.0.0.1:3310',
+	'username' => 'root',
+	'password' => '123456',
+	'database' => 'realmd',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
