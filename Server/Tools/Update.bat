7e5c9098465d0cc7a7ab94db07a5c7145ab3d7b0
Conan <conan_513@hotmail.com>
2018-04-14 19:55:24 +0200

    V3 update file

--- Server/Tools/Update.bat
+++ Server/Tools/Update.bat
@@ -15,6 +15,6 @@ cd spp-legion
 ..\git\cmd\git.exe add sql
 ..\git\cmd\git.exe commit -m "Fix git sql warning"
 cls
-..\git\cmd\git.exe pull https://github.com/conan513/spp-legion-patch.git 735-v2
+..\git\cmd\git.exe pull https://github.com/conan513/spp-legion-patch.git 735-v3
 echo.
 pause
