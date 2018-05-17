c04d6ceeba2475de0cd554c3334b562f1760e942
Conan <conan_513@hotmail.com>
2018-05-17 20:25:20 +0200

    Add more info for the updater

--- Server/Tools/Update.bat
+++ Server/Tools/Update.bat
@@ -7,6 +7,9 @@ set mod=%1
 taskkill /f /im bnetserver.exe
 taskkill /f /im worldserver.exe
 cls
+echo.
+echo Commit local changes before pull the update...
+echo.
 cd spp-legion
 ..\git\cmd\git.exe config user.name "SPP User"
 ..\git\cmd\git.exe config user.email sppuser@spp.com
@@ -15,6 +18,9 @@ cd spp-legion
 ..\git\cmd\git.exe add sql
 ..\git\cmd\git.exe commit -m "Fix git sql warning"
 cls
+echo.
+echo Downloading the latest update...
+echo.
 ..\git\cmd\git.exe pull https://github.com/conan513/spp-legion-patch.git 735-v3
 echo.
 pause
