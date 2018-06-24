a20d9d150161f56a89cb5de5961dabf1554dc723
Conan <conan_513@hotmail.com>
2018-06-24 07:48:06 +0200

    Pull from AshamaneCore + Custom
    AshamaneCore:
    - Spells/Priest Fix Shadow Mend
    - Improvements for mardum, texts, spells, sounds and more
    - Add missing npc in hallwarrior, eye of odyn
    - Fix human staring zone, organize the quest line according to blizzard and add new scripts
    
    Custom
    - Fix the missing Premium characters sql (script is unfinished yet)
    - Speed up the update process

--- Server/Tools/Update.bat
+++ Server/Tools/Update.bat
@@ -16,8 +16,6 @@ cd "%mainfolder%\spp-legion"
 ..\git\cmd\git.exe config user.email sppuser@spp.com
 ..\git\cmd\git.exe add Realms
 ..\git\cmd\git.exe commit -m "Custom Changes"
-..\git\cmd\git.exe add sql
-..\git\cmd\git.exe commit -m "Fix git sql warning"
 cls
 echo.
 echo Downloading the latest update...
