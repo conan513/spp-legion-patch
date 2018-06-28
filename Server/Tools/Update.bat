9bcb1c4a4244afa520746f4d4ad370c4ebf59706
Conan <conan_513@hotmail.com>
2018-06-28 17:05:55 +0200

    Improve custom script loader (console)

--- Server/Tools/Update.bat
+++ Server/Tools/Update.bat
@@ -9,11 +9,14 @@ taskkill /f /im bnetserver.exe
 taskkill /f /im worldserver.exe
 cls
 echo.
-echo Commit local changes before pull the update...
+echo Create local git config file...
 echo.
 cd "%mainfolder%\spp-legion"
 ..\git\cmd\git.exe config user.name "SPP User"
 ..\git\cmd\git.exe config user.email sppuser@spp.com
+echo.
+echo Add realm settings to git system...
+echo.
 ..\git\cmd\git.exe add Realms
 ..\git\cmd\git.exe commit -m "Custom Changes"
 cls
