e129ebb9b0c0ed96c4ab7c2cb0dfc28b98c5462b
Conan <conan_513@hotmail.com>
2018-08-19 00:36:27 +0200

    Custom fixes by Warlord Zul'Jin
    - World/DB Happy Raiders quest fix
    - World/DB Miners Fortune quest fix

---
+++ sql/custom/world/2018_08_19_00_Harpy_Raiders.sql
@@ -0,0 +1,29 @@
+DELETE FROM creature_loot_template WHERE entry = 3276 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3276, 5064, 96, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3277 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3277, 5064, 96, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3280 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3280, 5064, 96, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3278 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3278, 5064, 96, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3279 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3279, 5064, 96, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3452 and item = 5064;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3452, 5064, 96, 1, 1, 1);
---
+++ sql/custom/world/2018_08_19_01_Miners_Fortune.sql
@@ -0,0 +1,14 @@
+DELETE FROM creature_loot_template WHERE entry = 3286 and item = 5097;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3286, 5097, 100, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 9336 and item = 5097;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(9336, 5097, 100, 1, 1, 1);
+
+DELETE FROM creature_loot_template WHERE entry = 3283 and item = 5097;
+INSERT INTO creature_loot_template (entry, item, Chance, QuestRequired, MinCount,
+MaxCount) VALUES
+(3283, 5097, 100, 1, 1, 1);
