6985f59a4b7b4901e6ec40e04d38d2ae144991cc
wormogo <pehe.teszt@gmail.com>
2018-08-28 17:12:17 +0200

    Text and Broadcast Fixes in Gilneas City by Wormogo
    -Constant text and sound removed from creatures whic came from sniff
    -Broadcast texts connected to several NPC in creature_text

---
+++ sql/custom/world/2018_08_28_01_Constant_sound_fixed_and_Worgen_Texts_connected_broadcast.sql
@@ -0,0 +1,35 @@
+-- Broadcast addition to have data for further work in creature text
+
+UPDATE creature_text SET sound = 0 WHERE Sound = 19634 AND creatureID <> 35906;
+
+UPDATE creature_text SET BroadcastTextID = 36618 WHERE creatureID = 34571;
+UPDATE creature_text SET BroadcastTextID = 39313 WHERE creatureID = 34851;
+UPDATE creature_text SET BroadcastTextID = 44235 WHERE creatureID = 34913;
+UPDATE creature_text SET BroadcastTextID = 35240 WHERE creatureID = 34981;
+UPDATE creature_text SET BroadcastTextID = 38602 WHERE creatureID = 35112;
+UPDATE creature_text SET BroadcastTextID = 38601 WHERE creatureID = 35115;
+UPDATE creature_text SET BroadcastTextID = 35498 WHERE creatureID = 35230;
+UPDATE creature_text SET BroadcastTextID = 38739 WHERE creatureID = 35369;
+UPDATE creature_text SET BroadcastTextID = 38744 WHERE creatureID = 35378;
+UPDATE creature_text SET BroadcastTextID = 36092 WHERE creatureID = 35550;
+UPDATE creature_text SET BroadcastTextID = 36091 WHERE creatureID = 35550;
+UPDATE creature_text SET BroadcastTextID = 36038 WHERE creatureID = 35836;
+UPDATE creature_text SET BroadcastTextID = 36039 WHERE creatureID = 35836;
+UPDATE creature_text SET BroadcastTextID = 36094 WHERE creatureID = 35840;
+UPDATE creature_text SET BroadcastTextID = 36078 WHERE creatureID = 35905;
+UPDATE creature_text SET BroadcastTextID = 38729 WHERE creatureID = 35907;
+UPDATE creature_text SET BroadcastTextID = 36617 WHERE creatureID = 36132;
+UPDATE creature_text SET BroadcastTextID = 36209 WHERE creatureID = 36207;
+UPDATE creature_text SET BroadcastTextID = 36312 WHERE creatureID = 36231;
+UPDATE creature_text SET BroadcastTextID = 36310 WHERE creatureID = 36231;
+UPDATE creature_text SET BroadcastTextID = 36315 WHERE creatureID = 36231;
+UPDATE creature_text SET BroadcastTextID = 36336 WHERE creatureID = 36330;
+UPDATE creature_text SET BroadcastTextID = 36344 WHERE creatureID = 36331;
+UPDATE creature_text SET BroadcastTextID = 36340 WHERE creatureID = 36332;
+UPDATE creature_text SET BroadcastTextID = 36341 WHERE creatureID = 36332;
+UPDATE creature_text SET BroadcastTextID = 50192 WHERE creatureID = 50415;
+UPDATE creature_text SET BroadcastTextID = 50264 WHERE creatureID = 50474;
+
+
+
+-- End of Broadcast section
