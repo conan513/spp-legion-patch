9f5b6f7164a5e5e3527267bbf0131f8e5def46ed
wormogo <pehe.teszt@gmail.com>
2018-09-12 21:02:50 +0200

    Various_Fixes_SPP_Devs
    Enjoy our latest fixes!
    
    Varioues fixes:
    Mottled boar NPC flag corrected - by Fatliner
    Surf Crawler Until Flag corrected - by Fatliner
    
    Worgen Zone fixes - by Monad
    Phase fixes from Duskhaven to Tempest Reach - by Monad
    Map Change fix under Duskhaven (Cataclysm change) - by Monad
    
    Thank you @Monady, @Fatliner
    
    Co-Authored-By: monady <monady@users.noreply.github.com>
    Co-Authored-By: fatliner <fatliner@users.noreply.github.com>

---
+++ sql/custom/world/2018_09_12_00_correct_npcflag_for_Mottled_Boar_3098.sql
@@ -0,0 +1,2 @@
+-- fix - corect npcflag for Mottled Boar - 3098
+UPDATE `creature_template` SET `npcflag`=0 WHERE `entry`=3098;
---
+++ sql/custom/world/2018_09_12_01_correct_unit_flags_for_Surf_Crawler_3106.sql
@@ -0,0 +1,2 @@
+-- fix - corect unit_flags for Surf Crawler - 3106
+UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry`=3106;
