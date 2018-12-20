56cbd63965c871fff3e7f7ec1b9dabb983d7dc08
ConanHUN <conan_513@hotmail.com>
2018-12-20 10:04:34 +0100

    Update Update_fix_if_error.bat

--- Server/Tools/Update_fix_if_error.bat
+++ Server/Tools/Update_fix_if_error.bat
@@ -4,10 +4,34 @@ TITLE %NAME%
 COLOR 0A
 set mod=%1
 
+:menu
 taskkill /f /im bnetserver.exe
 taskkill /f /im worldserver.exe
+taskkill /f /im spp-httpd.exe
+taskkill /f /im node.exe
 cls
-cd spp-legion
+echo #######################################################
+echo # Single Player Project - Server Launcher              #
+echo # https://www.patreon.com/conan513                    #
+echo #######################################################
+echo.
+echo 1 - Reset AshamaneCore
+echo 2 - Reset BestCore (made by Thordekk)
+echo.
+set /P menu=Enter a number: 
+if "%menu%"=="1" (goto ashamanecore)
+if "%menu%"=="2" (goto bestcore)
+
+:ashamanecore
+cls
+cd "%mainfolder%\spp-legion"
+..\git\cmd\git.exe reset --hard
+echo.
+pause
+
+:bestcore
+cls
+cd "%mainfolder%\spp-bestcore"
 ..\git\cmd\git.exe reset --hard
 echo.
 pause
