7d4365f13181e27b94f147977ed6041296d2f2e5
ConanHUN <conan_513@hotmail.com>
2018-12-23 12:15:55 +0100

    Fix 2018_04.19_00_auth_worldchat.sql and 2018_04.19_00_auth_completequest_command.sql

--- sql/ashamane/auth/2018_04.19_00_auth_completequest_command.sql
+++ sql/ashamane/auth/2018_04.19_00_auth_completequest_command.sql
@@ -1,5 +1,5 @@
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
+REPLACE INTO `rbac_permissions` (`id`, `name`) VALUES
 (1016, 'Command: completequest');
 
-INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
+REPLACE INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
 (195, 1016);
