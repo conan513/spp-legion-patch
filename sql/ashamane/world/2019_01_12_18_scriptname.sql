bd7bfef6518c1cadb32aeff520ac3c9f50aac989
ConanHUN <conan_513@hotmail.com>
2019-01-13 23:17:49 +0100

    Pull from BestCore
    - Add more spell_dk
    - 2019_01_12_18_scriptname
    - 2019_01_12_19_Scriptname_ DB_error
    - Fixed DBErrors.log

---
+++ sql/ashamane/world/2019_01_12_18_scriptname.sql
@@ -0,0 +1,19 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (49143,207126,207142,114556,116888,81136,49028,206940,207346,61257,155166,227869,227870,55095,206930,212744,219809);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(49143, 'spell_dk_frost_strike'),
+(207126, 'spell_dk_icecap'),
+(207142, 'spell_dk_avalanche'),
+(114556, 'spell_dk_purgatory'),
+(116888, 'spell_dk_shroud_of_purgatory'),
+(81136, 'spell_dk_crimson_scourge'),
+(49028, 'spell_dk_dancing_rune_weapon'),
+(206940, 'spell_dk_mark_of_blood'),
+(207346, 'spell_dk_necrosis'),
+(61257, 'spell_dk_pvp_4p_bonus'),
+(155166, 'spell_dk_breath_of_sindragosa_damage'),
+(227869, 'spell_dk_six_feather_fan'),
+(227870, 'spell_dk_six_feather_fan_damage'),
+(55095, 'spell_dk_frost_fever'),
+(206930, 'spell_dk_heart_strike'),
+(212744, 'spell_dk_soulgorge'),
+(219809, 'spell_dk_tombstone');
