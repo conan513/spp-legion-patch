d58430e987d433b7fdd901215933e774788c8c88
Warlord Zul'Jin <42502821+takasi689@users.noreply.github.com>
2019-07-26 23:03:57 +0200

    Delete Test Flag Quest Objectives.sql

--- sql/ashamane/world/Test Flag Quest Objectives.sql
+++
@@ -1,6 +0,0 @@
--- Unexpected Results Quest 8488
-UPDATE `world`.`quest_objectives` SET `Flags`='1', `Description`='Gharsul the Remorseless slain' WHERE  `ID`=289860;
-
--- Research Notes Quest 9255
-UPDATE `world`.`quest_objectives` SET `Flags`='1', `Flags2`='0', `Description`='Research Notes (Provided)' WHERE  `ID`=262275;
-
