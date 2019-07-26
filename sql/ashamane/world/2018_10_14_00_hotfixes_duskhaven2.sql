ee834a51accfdf70ac5b509d5e7b0003870adba8
Warlord Zul'Jin <42502821+takasi689@users.noreply.github.com>
2019-07-27 00:55:14 +0200

    Delete 2018_10_14_00_hotfixes_duskhaven2.sql

--- sql/ashamane/world/2018_10_14_00_hotfixes_duskhaven2.sql
+++
@@ -1,8 +0,0 @@
-/*=====================
--- Hotfixes Duskhaven 2
-=====================*/
-DELETE FROM `phase_x_phase_group` WHERE `ID` IN (1030,1031);
-INSERT INTO `phase_x_phase_group` (`ID`, `PhaseID`, `PhaseGroupID`, `VerifiedBuild`) VALUES 
-(1030, 186, 1006, 26365),
-(1031, 187, 1006, 26365);
-
